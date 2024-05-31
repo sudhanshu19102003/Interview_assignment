9cb6189b6f4ba6dd2bf5a82bd46c98e4acf327b4
sudhanshu-19102003 <sudhanshu19102003@gmail.com>
2024-05-31 17:04:45 +0530

    modified:   Interview_Assignment/Interview_Assignment/__pycache__/settings.cpython-39.pyc 	modified:   Interview_Assignment/Interview_Assignment/urls.py 	modified:   Interview_Assignment/staticfiles/admin/css/base.css 	modified:   Interview_Assignment/staticfiles/admin/css/changelists.css

--- Interview_Assignment/Interview_Assignment/urls.py
+++ Interview_Assignment/Interview_Assignment/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('data_summery.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
